93f58eb9baba11d91884b76d5ee36ae5dd1eb1a4
BarnyBaron <143497222+BarnyBaron@users.noreply.github.com>
2024-03-26 15:55:52 +0000

    Updated index.js to new Function URL

--- website/index.js
+++ website/index.js
@@ -1,7 +1,7 @@
 // Javascript Code
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://ystbokglls45kedledujzbtobe0kaity.lambda-url.eu-west-2.on.aws/");
+    let response = await fetch("https://wpugjl5v6vgf4ldfs32flr35ey0tayde.lambda-url.eu-west-2.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
